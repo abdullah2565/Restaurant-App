dd6ca6bac912b7d4f2cdaf184e393c5bc1d97a73
Abdullah khalid <127050435+abdullah2565@users.noreply.github.com>
2023-10-29 05:46:24 +0500

    till loader

---
+++ main.js
@@ -0,0 +1,18 @@
+let bodyy = document.getElementById('bodydiv')
+
+let btnn = document.getElementById('explorebtn')
+
+let loder = document.getElementById('loder')
+
+let main = document.getElementById('main')
+
+btnn.addEventListener('click' , function loader(){
+  main.style.display= 'none';
+  loder.style.display = 'flex'
+  bodyy.style.backgroundColor = ' rgba(0, 0, 0, 0.9)';
+
+  setTimeout(function(){
+    //  window.location.href = 'burger.html'
+
+  },2000)
+})
